c2cbaa211cbd47decb38d026c2acb27b1233666c
Ntsakelo Makhuvele <ntsakelo.makhuvele>
2023-10-19 23:57:23 +0200

    Tests added

--- src/Ferry.ts
+++ src/Ferry.ts
@@ -3,7 +3,8 @@ import Car from "./Car";
 export default class Ferry{
     public readonly carsLimit;
     public readonly passengerLimit;
-    
+    public readonly carColors: Array<string> = [];
+    public readonly carRegs: Array<string> =[];
     private people_count= 0;
     private car_count = 0;
 
@@ -14,19 +15,43 @@ export default class Ferry{
   public get carCount() {
     return this.car_count;
   }  
+  public get carRegList(){
+    return this.carRegs
+  }
+  public regCount(reg:string){
+   let count = 0;
+   for(let i = 0; i < this.carRegs.length; i++){
+       if(reg === this.carRegs[i]){
+         count++;
+       }
+      }
+      return count;
+  }
     constructor(carsLimit: number, passengerLimit: number){
         this.carsLimit = carsLimit;
         this.passengerLimit = passengerLimit;     
     };
 
     public board(car: Car) {
-      if(this.carCount < this.carsLimit && this.peopleCount < this.passengerLimit){
-        this.car_count++;
-        this.people_count = car.passengerCount;
-        return "Accepted";
-      }
-     else if(this.carCount >= this.carsLimit) {
+      if(this.carCount >= this.carsLimit || this.peopleCount + car.passengerCount > this.passengerLimit){
         return "Rejected";
+      }
+      else{
+        this.carColors.push(car.color);
+        this.carRegs.push(car.registration);
+        this.car_count += 1;
+        this.people_count += car.passengerCount;
+        let regCount = this.regCount(car.registration);
+        //console.log(regCount);
+        if(regCount > 3 && regCount < 7){
+          return "Half price"
+        }
+       if(regCount > 7){
+          return "You go free"
+        }
+         if(regCount < 3){
+          return "Accepted";
+        }
      }
     }
 }
--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,93 @@
 import assert from "assert";
+import Ferry from "../src/Ferry";
+import Car from "../src/Car";
 
+describe("People count", ()=>{
+    it("should be able to count the number of people in the ferry",() =>{
+        const benz = new Car("black", 4, "CF 20001");
+        
+        const ferry = new Ferry(6, 6);
+        
+        ferry.board(benz);
+        
+        assert.equal(4,ferry.peopleCount);
+    })
+    
+    it("should be able to count the number of people in the ferry",() =>{
+        const bmw = new Car("Blue", 2, "CA 10000");
+        const corolla = new Car("Red", 2, "GP 11111");
+        const ferry = new Ferry(1,6);
+        ferry.board(bmw);
+        ferry.board(corolla);
+        assert.equal(2,ferry.peopleCount);
+    })
+})
+describe("Cars count", () => {
+    it("should be able to count the number of cars in the ferry",()=>{
+        const audi = new Car("Green", 4, "CJ 123111");
+        const ferry = new Ferry(3,6);
+        ferry.board(audi);
+        assert.equal(1, ferry.carCount)
+    })
+    it("should be able to count the number of cars in the ferry",()=>{
+        const audi = new Car("Green", 4, "L 123123");
+        const taxi = new Car("yellow",15, "CY 123111");
+        const polo = new Car("Silver", 2, "CL 111111");
+        
+        const ferry = new Ferry(3,6);
+        ferry.board(audi);
+        ferry.board(taxi);
+        ferry.board(polo);
+        assert.equal(2, ferry.carCount)
+    })
+})
+describe("board method", () => {
+    it("should return accepted if ferry is not full",() =>{
+        const ferrarri = new Car("black", 2,"CA 111 111");
+        const ferry = new Ferry(2,3);
+        assert.equal("Accepted", ferry.board(ferrarri));
 
+    })
+    it("should return rejected if ferry has enough cars",() =>{
+        const tazz = new Car("Brown", 4,"CA 111 111");
+        const corolla = new Car("Red", 4, "CF 333-111");
+        const ferrarri = new Car("Black", 2, "CA 441-000");
+        
+        const ferry = new Ferry(2,20);
+        ferry.board(tazz);
+        ferry.board(corolla);
+        assert.equal("Rejected", ferry.board(ferrarri));
 
+    })
+    it("should return \"half price\" if car has more than 3 trips",() =>{
+        const tazz = new Car("Brown", 4,"CA 111 111");
+        const tazz1 =  new Car("Brown", 4,"CA 111 111");
+        const tazz2 = new Car("Brown", 4,"CA 111 111");
+        const tazz3 = new Car("Brown", 4,"CA 111 111");
+        
+        const ferry = new Ferry(5,20);
+        ferry.board(tazz);
+        ferry.board(tazz1);
+        ferry.board(tazz2);
+        assert.equal("Half price", ferry.board(tazz3));
+    })
+    it("should return \"you go free\" if car has more than 7 trips",() =>{
+        const tazz = new Car("Brown", 4,"CA 111 111");
+        const tazz1 =  new Car("Brown", 4,"CA 111 111");
+        const tazz2 = new Car("Brown", 4,"CA 111 111");
+        const tazz3 = new Car("Brown", 4,"CA 111 111");
+        const tazz4 = new Car("Brown", 4,"CA 111 111");
+        const tazz5 = new Car("Brown", 4,"CA 111 111");
+        const tazz6 = new Car("Brown", 4,"CA 111 111");
+        const tazz7 = new Car("Brown", 4,"CA 111 111");
+        const ferry = new Ferry(10,50);
+        ferry.board(tazz);
+        ferry.board(tazz1);
+        ferry.board(tazz2);
+        ferry.board(tazz3);
+        ferry.board(tazz4);
+        ferry.board(tazz5);
+        ferry.board(tazz6);
+        assert.equal("You go free", ferry.board(tazz7));
+    })
+})
